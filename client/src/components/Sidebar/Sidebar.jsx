1deddbc6e82642d9f9ad04bcc3db39db4fdc056d
alvinAdler <alvinadlersampow@gmail.com>
2022-08-08 21:51:34 +0800

    Overhauled the looks of the sidebar toggle button
    Addition: Added functionalities to the other buttons in the sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,33 @@
+import { useNavigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
 import { FiLogOut, FiUser, FiMenu } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 import "./Sidebar_master.scss"
 
-const Sidebar = () => {
-	return (
-		<div className="sidebar-container">
-			<header>
-				<button><img src="/publicImages/ChatAku_alt.svg" alt="ChatAku" /></button>
-				<button><FiLogOut/></button>
-				<button><FiUser/></button>
-				<button><FiMenu/></button>
-			</header>
-			<div className="chat-list">
+const Sidebar = ({isSidebarVisible, toggleSidebarVis}) => {
 
+	const navigate = useNavigate()
+
+	const logout = () => {
+		Cookies.remove("authToken")
+		navigate("/login", {replace: true})
+	}
+
+	return (
+		<aside className={`sidebar-container ${isSidebarVisible && "open"}`}>
+			<div className="main-sidebar">
+				<header>
+					<img src="/publicImages/ChatAku_alt.svg" alt="ChatAku" />
+					<button type="button" onClick={logout}><FiLogOut/></button>
+					<Link to="/friends"><FiUser/></Link>
+				</header>
+				<div className="chat-list">
+
+				</div>
 			</div>
-		</div>
+			<button type="button" onClick={toggleSidebarVis}><FiMenu/></button>
+		</aside>
 	)
 }
 
--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
+
 import "./Dashboard_master.scss"
 
 import Sidebar from "../../components/Sidebar/Sidebar"
 import ChatStarter from "../../components/ChatStarter/ChatStarter"
 
 const Dashboard = () => {
+
+	const [isSidebarVisible, setIsSidebarVisible] = useState(true)
+
 	return (
 		<div className="dashboard-container">
-			<Sidebar/>
+			<Sidebar isSidebarVisible={isSidebarVisible} 
+			toggleSidebarVis={() => {
+				console.log("I am clicked")
+				setIsSidebarVisible((prevState) => !prevState)
+			}}
+			/>
 			<div className="main-chat">
 				<ChatStarter isVisible={true}/>
 			</div>
