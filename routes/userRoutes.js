deca311626d1657184b66b01d7d2729a43b3745f
alvinAdler <alvinadlersampow@gmail.com>
2022-08-12 21:12:31 +0800

    Added an API endpoint to reject or to accept a friend request

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,10 @@ const UserModel = require("../models/UserModel")
 const tokenVerif = require("../middlewares/tokenVerif")
 
 const SALT_ROUND = 10
+const ALLOWED_REQUEST_METHOD = {
+    REJECT: "reject",
+    ACCEPT: "accept"
+}
 
 router.post("/register", async (req, res) => {
 
@@ -125,6 +129,12 @@ router.post("/sendRequest", tokenVerif, async (req, res) => {
         })
     }
 
+    //TODO: Do extra validations
+    /**
+     * Do not let the user send a request to the him/herself or to the other
+     * user who already accepted the friend request
+     */
+
     //Putting each user's ID into the other's request list
     try{
         const resCurrentUser = await UserModel.updateOne({_id: req.user._id}, {$addToSet: {requestList: targetUserId}})
@@ -142,6 +152,80 @@ router.post("/sendRequest", tokenVerif, async (req, res) => {
             message: "Unable to send request",
             err: `${err}`
         })
+    }
+})
+
+router.get("/getFriends", tokenVerif, async(req, res) => {
+
+})
+
+router.post("/handleRequestStatus", tokenVerif, async(req, res) => {
+    const { targetUserId, action } = req.body
+
+    if([targetUserId, action].some((item) => item === null || item === undefined)){
+        return res.status(400).json({
+            message: "Undefined or null element detected"
+        })
+    }
+
+    switch(action.toLowerCase()){
+        case ALLOWED_REQUEST_METHOD.ACCEPT:
+            //Move the users' ID from the request list to the friends list
+            try {
+                const resTargetUser = await UserModel.updateOne({_id: targetUserId, requestList: req.user._id}, {
+                    $push: {"friendsList": req.user._id},
+                    $pull: {"requestList": req.user._id}
+                })
+    
+                const resCurrentUser = await UserModel.updateOne({_id: req.user._id, requestList: targetUserId}, {
+                    $push: {"friendsList": targetUserId},
+                    $pull: {"requestList": targetUserId}
+                })
+
+                if([resTargetUser, resCurrentUser].every((item) => item.acknowledged && item.modifiedCount > 0)){
+                    return res.status(200).json({
+                        message: "Successfully accept the friend request"
+                    })
+                }
+
+                throw new Error("One process was not acknowledged")
+
+            } catch (err) {
+                return res.status(500).json({
+                    message: "Failed to accept the user's request",
+                    err: `${err}`
+                })
+            }
+
+        case ALLOWED_REQUEST_METHOD.REJECT:
+            //Delete the users' ID from the request list
+            try {
+                const resTargetUser = await UserModel.updateOne({_id: targetUserId, requestList: req.user._id}, {
+                    $pull: {"requestList": req.user._id}
+                })
+    
+                const resCurrentUser = await UserModel.updateOne({_id: req.user._id, requestList: targetUserId}, {
+                    $pull: {"requestList": targetUserId}
+                })
+
+                if([resTargetUser, resCurrentUser].every((item) => item.acknowledged && item.modifiedCount > 0)){
+                    return res.status(200).json({
+                        message: "Successfully rejected the friend request"
+                    })
+                }
+
+                throw new Error("One process was not acknowledged")
+
+            } catch (err) {
+                return res.status(500).json({
+                    message: "Failed to reject the user's request",
+                    err: `${err}`
+                })
+            }
+        default:
+            return res.status(400).json({
+                message: "Action is not valid"
+            })
     }
 })
 
